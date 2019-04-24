ecb58c958f1d4fe3c4bdb002db7b21f2955f8df8
MazenAyman13 <49705311+MazenAyman13@users.noreply.github.com>
2019-04-24 14:23:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4981,16 +4981,5 @@ client.on("message", message => {
   }
 });
 
-client.on('message', message => {
-if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith(prefix+'users')){
- if(!message.author.id === '570576806000852992') return;
-message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
 
 client.login(process.env.BOT_TOKEN);
